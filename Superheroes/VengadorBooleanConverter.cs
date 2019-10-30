93c9b2bca758798d674f36361d8be1fe0422c145
IsmaCollado <ismael.collado@iesdoctorbalmis.com>
2019-10-30 09:56:48 +0100

    Actualizacion de funciones

---
+++ Superheroes/VengadorBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Superheroes
+{
+    class VengadorBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if ((bool)value)
+                    return "si";
+                else
+                    return "no";
+            }
+            return "Error en convertidor";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valor = value.ToString();
+            if (valor == "si")
+                return true;
+            else
+                return false;
+        }
+    }
+}
